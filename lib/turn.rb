e1e6f1fe93fd378a4b69fd54751ae2c700ecf215
dsanalog <dsanalog@gmail.com>
2019-01-16 16:34:28 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+def valid_move?(board, index)
+  if(index > 8)
+    return FALSE
+  elsif position_taken?(board, index)
+    return FALSE
+  else
+    return TRUE
+  end
+end
+
+    
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return FALSE
+  else
+    return TRUE
+  end
+end
